de8ceb9e96a370f3841c9ff36242479d8bec306d
Yakup <yakupjoraev@gmail.com>
2024-09-23 01:27:39 +0300

    added new pages

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,19 @@
 // Custom Scripts
 // Custom scripts
 
+function slider() {
+  const container = document.querySelector('.slider-container');
+
+  if (!container) {
+    return null
+  }
+
+  const swiper = new Swiper('.main-slider', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+  });
+}
+
+slider();
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 // Custom scripts
+
+function slider() {
+  const container = document.querySelector('.slider-container');
+
+  if (!container) {
+    return null
+  }
+
+  const swiper = new Swiper('.main-slider', {
+    // Default parameters
+    slidesPerView: 1,
+    spaceBetween: 10,
+  });
+}
+
+slider();
